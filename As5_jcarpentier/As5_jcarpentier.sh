8daa6ac8072b886a43104613c04bc91596fc06e9
jac <jac@pop-os.localdomain>
2020-05-05 02:03:32 -0400

    added stuff

---
+++ As5_jcarpentier/As5_jcarpentier.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+function  SedGetter (){
+
+# As4_jcarpentier.awk salary_file.txt
+echo "What is your awk and txt file name";
+read awkFile txtFile;
+
+
+#  check if a file exists, if a file is readable or if the file has extension .awk
+ file1=".awk"
+if [[ -f ./"$awkFile" && -x ./"$awkFile" && ${file1: -4 } == ".awk" ]]; then 
+    echo "file exist is executable and ends with awk extension";
+else
+    echo "There is no file that exist or is executable or ends with awk extension";
+    exit 1
+fi
+
+#  check if a file exists, if a file is executable or if the file has extension .txt
+ file2=".txt"
+if [[ -f ./"$txtFile" && -r ./"$txtFile" && ${file2: -4} == ".txt" ]]; then 
+
+  echo "file exist is readable and ends with txt extension";
+else
+    echo "There is no file that exist or is readable or ends with txt extension";
+    exit 1
+fi
+awk -f $awkFile $txtFile;
+
+#awk -f $awkFile salary_file.txt;
+#awk -f $1 $2;
+#awk -f $awkFile $1; # find out how to pass file as positional parameter
+
+
+}
+
+function FuncCall (){
+echo
+echo "Solution to Assignment 4, Part 2"
+echo "================================"
+SedGetter
+zip -r As5_jcarpentier ../As5_jcarpentier
+}
+
+FuncCall
+
+
+
