d312c205274c407e06e05018f49a8f0e94027f96
jac <jac@pop-os.localdomain>
2020-05-13 19:23:38 -0400

    Edited alot

--- As5_jcarpentier/As5_jcarpentier.sh
+++ As5_jcarpentier/As5_jcarpentier.sh
@@ -1,79 +1,104 @@
 #!/bin/bash -e
 
-# Part 1 (75 pts)  Write a BASH script to create a user account.  
-#    The script should process two positional parameters:  
-#       o First positional parameter is supposed to be a string with user name (e.g., user_name) 
-#       o Second positional parameter is supposed to be a string with user password (e.g., user_password) 
-
-#    The script should be able to process one option (use getopts command): 
-#       o Option -f arg_to_ot_f that will make your script direct all its output messages to file  -arg_to_ot_f 
-
 # In your script: 
 # --------------
-#    Check if option -f was passed to your script and if it has its argument arg_to_ot_f 
-#       o If YES, continue your script according to an option that was specified (i.e., send all output messages of your script to arg_to_ot_f)  
-#    Check if two positional parameters were passed to your script when it was invoked 
-#       o If NOT, print an appropriate message and terminate your script with error (i.e., use exit 1)  
-#       o If YES, continue processing both positional parameters 
-#    Create a new user on the Linux computer/system on which your script is running (you may have to run your script with root privileges or using sudo command to do it) 
+#    Create a new user on the Linux computer/system on which your script is running
+#     (you may have to run your script with root privileges or using sudo command to do it)
 #       o User’s account should have name denoted by the first positional parameter (e.g., user_name) 
 #       o User’s account should have password denoted by the second positional parameter (e.g., user_password). 
-#         Note that when assigning a password to a user, it must be in encrypted form – research how to do it correctly) 
-#    Check if a new directory (e.g., user_name) was crated for the user under /home/ directory 
-#       o If NOT, print an appropriate message and terminate your script with error  
-#       o If YES, continue to the next step 
-#    Check if the new user was added to the list of the users by checking /etc/passwd file 
-#     (use grep command with user_name and find out if the line in it for the user was created) 
-#       o If NOT, print an appropriate message and terminate your script with error  
-#       o If YES, print an appropriate message that the user was successfully added and terminate your script with success 
-#         (i.e., use exit 0) 
-      
+#         Note that when assigning a password to a user, it must be in encrypted form – research how to do it
+#          correctly) 
 
-Username=$1;
-Pass=$2;
-# positional parameter range from $0 to $9
-# function SedGetter (){
 
-while getopts f: opt_var1; do
-    case $opt_var1 in
-          f) echo "Username & Password is ${@}" >&2 ;;# displays all param passed 
+# 1) check also if there are exactly 2 non-empty positional parameters
 
-#-a|--action) sudo adduser --home /home/$Username $Username ;;
-        #f|--action) echo "Username & Password is ${@}" sudo adduser --home /home/$Username $Username >&2 ;;
+# 4) you have to retrieve argument that was passed to option -f somewhere in or after line  29   
+               # if [[ ! $@ =~ ^\-.+ && $# > 1 ]];then 
 
-         \?) echo "Invalid option: ${OPTARG}" >&2  ;; # store option not given     
-          :)  echo "Option -${OPTARG} requires an argument." >&2 ;; # f no argument is provided getopts 
-          # will set opt to :. We can recognize this error condition by catching the : case and printing 
-          # an appropriate error message.
-    esac
-done
+function  ErrorChecker(){
+  echo "Error"
+  exit 1
+}
+ opt_used="false"
 
-echo "Out of the getopts loop. OPTIND is now $OPTIND";
-shift "$((OPTIND -1))"
-if [ $# > 1 ]; then
-echo "Number of param $# "; # displays count of param
-# echo "$0"; # prints shell script name which is at postion zero
-# Create user in home dir and print it and delete and print again
-sudo adduser --home /home/$Username $Username; # how to pass password=
-ls /home;
-sudo userdel -r $Username;
-ls /home;
+if [[ ! $@ =~ ^\-.+ ]];then 
+ echo "Missing -f flag"
 fi
+ while getopts ":f:" opt_var1; do
+     case $opt_var1 in
+            f) echo -n "Option $1 was passed "
+               if [[ $# -lt 3 ]]; then 
+                    echo "Not enough parameter less than three" 
+                    ErrorChecker
+                elif [[  $# -gt 2 ]];then
+                  echo "total number of param $# "
+                  # positional parameter range from $0 to $9
+                  # echo "$0"; # prints shell script name which is at postion zero
+                fi
+                echo "Argument for Username & Password passed & are $2 & $3" >&2 
+                opt_used="true"
+                opt_file=$OPTARG
+                Username=$2
+                Pass=$3
+                ;;# displays all param passed 
+               
+
+
+          \?) echo "Invalid option: ${OPTARG}" >&2  ;; # store option not given     
+           :) echo "Option -${OPTARG} requires an argument." >&2 ;; # f no argument is provided getopts 
+           # will set opt to :. We can recognize this error condition by catching the : case and printing 
+           # an appropriate error message.
+     esac
+ done
+
+
+
+
+# Create user with encrypted password
+# sudo useradd -m -p $(openssl passwd -1 ${Pass}) -s /bin/bash -d ${Username}
+
+# sudo useradd -c "$Username" -d /home/$Username -m -U -s /bin/bash -k /etc/skel $Username -p $Pass
+# echo $Pass | openssl enc -base64 -e -aes-256-cbc -pbkdf2 -iter 100000 -nosalt  -pass pass:garbageKey # changes passwd
+# decrypt
+# echo $Pass | openssl enc -base64 -d -e -aes-256-cbc -pbkdf2 -iter 100000 -nosalt  -pass pass:garbageKey 
+# #sudo passwd $Username
+#  ls /home;
+#  sudo userdel -r $Username; # test purposes
+#  ls /home;
+
+if [[ -d "/home/$Username" ]] && grep -Fq "$Username" /etc/passwd; then
+  echo "$Username was created in home dir and exist in passwd "
+else 
+  echo "$Username Doesnt exist in home or /etc/passwd "
+  ErrorChecker
+fi  
+
 #zip -r As5_jcarpentier ../As5_jcarpentier
-#}
-#SedGetter "$@" "$#" 
- 
-# Part 2 (20 pts) Write a short SAD program ( a set of SED instructions) that perform following 
-# operations on a long_file.txt file (you can download it from Blackboard -> Useful Links). Your SED program should: 
-#    Remove, from long_file.txt all lines starting with strings A, An and The (i.e., Capital A at the beginning of the line, 
-#     string An and the beginning of the line and string The at the beginning of the line)  
-#    Then, substitute all hyphened worlds (e.g., Unix-like, multi-user) with double hyphened words 
-#     (e.g., Unix-like becomes Unix—like and multi-user becomes multi--user) 2)  
-#    Insert current date and your name after the last line of long_file.txt 
-#    Save edited file as  long_file.txt.new by adding .new to the name of the file 
 
-# Part 3 (5 pts) 
-#    Log as a user that you created in Part 1 of the assignment using user_name and user_password. If you successfully logged 
-#     to the new account, write “Yes, I successfully logged in as the new user” as a note when submitting it in Blackboard. 
+# if grep -Fq "$Username" /etc/passwd
+# then
+#     echo "$Username exist"
 
-# Functions affect positional parameters so create outside function
+# else
+#     echo "$Username doesnt exist"
+#     exit 1
+# fi
+# if awk -F: /$Username/ /etc/passwd; then
+#   echo "$Username exist"
+# else 
+#   echo "$Username doesnt exist"
+#   exit 1
+# fi
+
+# can be done in awk 
+sed -r -i 's/^\b(An|A|The)\b//gI' long_file.txt
+sed -r -i 's/-/--/g' long_file.txt
+#dat=`date "+%D" `
+# sed -ri "1s#(.*)#${dat}\1#" long_file.txt
+#sed -i -e '$s/$/Joshua Carpentier/' long_file.txt
+#sed -i "s/\$/ $var/" long_file.txt 
+echo >> long_file.txt
+echo "Joshua Carpentier" >> long_file.txt
+date >> long_file.txt
+cp long_file.txt long_file.txt.new
+
